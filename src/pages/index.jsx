afd837caca77ae106b95957d3c4356e96278d4f1
Safvan Avcı <43886857+safvanavci@users.noreply.github.com>
2023-01-16 18:30:44 +0300

    Update index.jsx

---
+++ src/pages/index.jsx
@@ -0,0 +1,4 @@
+export { default as Home } from "./Home";
+export { default as Login } from "./Login";
+export { default as Product } from "./Product";
+export { default as Register } from "./Register";
